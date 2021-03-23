336c7ef07f4af4dc89e22b8b3f581f17bd6def32
Luis Velásquez <luvejo.dev@gmail.com>
2021-03-23 17:43:18 -0500

    feat(page): add NotFound page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/pages/HomePage'
 import ThreadShowPage from '@/pages/ThreadShowPage'
+import NotFoundPage from '@/pages/NotFoundPage'
+import srcData from '@/data.json'
 
 const routes = [
   {
@@ -13,7 +15,28 @@ const routes = [
     path: '/threads/:id',
     name: 'ThreadShow',
     component: ThreadShowPage,
-    props: true
+    props: true,
+    beforeEnter: (to, from, next) => {
+      const threadExists = srcData.threads.find(
+        thread => thread.id === to.params.id
+      )
+
+      if (threadExists) {
+        return next()
+      }
+
+      next({
+        name: 'NotFound',
+        params: { pathMatch: to.path.substring(1).split('/') },
+        query: to.query,
+        hash: to.hash
+      })
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFoundPage
   }
 ]
 
